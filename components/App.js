be21ba266874c7ac33e9ffed0b43c81ba0fdda2d
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-08 16:03:45 +0100

    Fix Contacs.js

--- components/App.js
+++ components/App.js
@@ -1,3 +1,31 @@
+var contacts = [
+    {
+      id: 1,
+      firstName: 'Jan',
+      lastName: 'Nowak',
+      email: 'jan.nowak@example.com',
+    },
+    {
+      id: 2,
+      firstName: 'Adam',
+      lastName: 'Kowalski',
+      email: 'adam.kowalski@example.com',
+    },
+    {
+      id: 3,
+      firstName: 'Zbigniew',
+      lastName: 'Koziol',
+      email: 'zbigniew.koziol@example.com',
+    }
+  ];
+
+  var contactForm = {
+    firstName: '',
+    lastName: '',
+    email: ''
+  };
+
+
 var App = React.createClass({
     render: function () {
         return (
