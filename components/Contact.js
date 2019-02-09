2bb7515d897e9d74175cc55bc0fedfcc93a254c2
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-09 17:49:00 +0100

    Convert to JSX

--- components/Contact.js
+++ components/Contact.js
@@ -5,17 +5,18 @@ var Contact = React.createClass({
 
     render: function () {
         return (
-            React.createElement('div', { className: 'contactItem' },
-                React.createElement('img', {
-                    className: 'contactImage',
-                    src: 'http://icons.veryicon.com/png/System/Onebit%204/contact%20vcard.png'
-                }),
-                React.createElement('p', { className: 'contactLabel' }, 'Imię: ' + this.props.item.firstName),
-                React.createElement('p', { className: 'contactLabel' }, 'Nazwisko: ' + this.props.item.lastName),
-                React.createElement('a', { className: 'contactEmail', href: 'mailto:' + this.props.item.email },
-                    this.props.item.email
-                )
-            )
+            <div className='contactItem'>
+                <img className='contactImage' src='http://icons.veryicon.com/png/System/Onebit%204/contact%20vcard.png'/>
+                <p className='contactLabel'>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className='contactLabel'>
+                    Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className='contactEmail' href={'mailto:' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
         )
     },
 });
